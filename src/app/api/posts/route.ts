f39031c93994920f53138834e2eaffd6d9b20328
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-07-29 23:47:30 +0530

    did something

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,12 +1,28 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+const prisma = new PrismaClient();
 
 export async function GET() {
-  const prisma = new PrismaClient();
-
-  const user = await prisma.post.findMany({
+  const posts = await prisma.post.findMany({
     orderBy: { postNumber: "desc" },
   });
 
-  return NextResponse.json({ user });
+  return NextResponse.json({ posts }, { status: 200 });
+}
+
+export async function POST(data: Request) {
+  try {
+    const userList = await data.json();
+    const users = await prisma.user.createMany({ data: userList });
+    console.log(users);
+
+    return NextResponse.json("OK");
+  } catch (error) {
+    return NextResponse.json(
+      { error: error },
+      {
+        status: 409,
+      }
+    );
+  }
 }
