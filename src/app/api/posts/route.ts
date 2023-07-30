998607eb24341d022314d95061670aa486b52620
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-07-31 04:38:41 +0530

    made schema

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,37 +1,37 @@
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
-const prisma = new PrismaClient();
+// import { PrismaClient } from "@prisma/client";
+// import { NextResponse } from "next/server";
+// const prisma = new PrismaClient();
 
-export async function GET() {
-  try {
-    const posts = await prisma.post.findMany({
-      orderBy: { postNumber: "desc" },
-    });
+// export async function GET() {
+//   try {
+//     const posts = await prisma.post.findMany({
+//       orderBy: { postNumber: "desc" },
+//     });
 
-    return NextResponse.json({ posts }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({
-      errorMessage: "this is an error message",
-    });
-  }
-}
+//     return NextResponse.json({ posts }, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({
+//       errorMessage: "this is an error message",
+//     });
+//   }
+// }
 
-export async function POST(data: Request) {
-  try {
-    const userList = await data.json();
-    const users = await prisma.user.createMany({ data: userList });
-    console.log(users);
+// export async function POST(data: Request) {
+//   try {
+//     const userList = await data.json();
+//     const users = await prisma.user.createMany({ data: userList });
+//     console.log(users);
 
-    return NextResponse.json("OK");
-  } catch (error) {
-    return NextResponse.json(
-      {
-        error: error,
-        errorMessage: "this is a error mesage,sorry for inconvinience",
-      },
-      {
-        status: 409,
-      }
-    );
-  }
-}
+//     return NextResponse.json("OK");
+//   } catch (error) {
+//     return NextResponse.json(
+//       {
+//         error: error,
+//         errorMessage: "this is a error mesage,sorry for inconvinience",
+//       },
+//       {
+//         status: 409,
+//       }
+//     );
+//   }
+// }
